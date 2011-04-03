10301710742846b5c3ddd0e76db694cf7030d06d
Sidorov Aleksey <sauron@citadelspb.com>
2011-04-04 02:54:45 +0400

    Hover effects

--- sevenlist/src/framebutton.cpp
+++ sevenlist/src/framebutton.cpp
@@ -1,8 +1,25 @@
 #include "framebutton.h"
+#include <QGraphicsDropShadowEffect>
+#include <QMouseEvent>
+#include <QMessageBox>
+
+class FrameButtonPrivate
+{
+public:
+	QGraphicsDropShadowEffect effect;
+};
 
 FrameButton::FrameButton(QWidget *parent) :
-    QPushButton(parent)
+	QPushButton(parent), d_ptr(new FrameButtonPrivate)
 {
+	Q_D(FrameButton);
+	d->effect.setColor(QColor(255, 50, 50));
+	d->effect.setBlurRadius(22);
+	d->effect.setOffset(1);
+	d->effect.setEnabled(false);
+	setGraphicsEffect(&d_func()->effect);
+
+
 	setMaximumHeight(20);
 	setMinimumSize(35, 20);
 	setStyleSheet(("QPushButton { "
@@ -12,3 +29,22 @@ FrameButton::FrameButton(QWidget *parent) :
 
 	//setIconSize(QSize(12,12));
 }
+
+FrameButton::~FrameButton()
+{
+
+}
+
+void FrameButton::enterEvent(QEvent *e)
+{
+	d_func()->effect.setEnabled(true);
+	QPushButton::enterEvent(e);
+}
+
+void FrameButton::leaveEvent(QEvent *e)
+{
+	d_func()->effect.setEnabled(false);
+	QPushButton::leaveEvent(e);
+}
+
+
--- sevenlist/src/framebutton.h
+++ sevenlist/src/framebutton.h
@@ -3,11 +3,19 @@
 
 #include <QPushButton>
 
+class FrameButtonPrivate;
 class FrameButton : public QPushButton
 {
     Q_OBJECT
+	Q_DECLARE_PRIVATE(FrameButton)
 public:
 	explicit FrameButton(QWidget *parent = 0);
+	virtual ~FrameButton();
+protected:
+	void enterEvent(QEvent *);
+	void leaveEvent(QEvent *);
+private:
+	QScopedPointer<FrameButtonPrivate> d_ptr;
 };
 
 #endif // FRAMEBUTTON_H
